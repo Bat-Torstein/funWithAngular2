9056a1ceb6f25e6a54bac7454b22b1aa312a2879
Torstein Bjørgan <tob@itema.no>
2016-08-12 21:55:32 +0200

    added angular2

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'testApp',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { } import { Component } from '@angular/core';
+@Component({
+    selector: 'testApp',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
