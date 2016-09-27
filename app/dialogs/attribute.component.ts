9cf2e2b09780147e08409b260bb89d5247129205
Torstein Bjørgan <tob@itema.no>
2016-09-28 00:25:11 +0200

    styling

--- app/dialogs/attribute.component.ts
+++ app/dialogs/attribute.component.ts
@@ -1,18 +1,22 @@
-﻿import { Component, Input} from '@angular/core';
+﻿import { Component, Input, OnInit} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AttributeModel } from './attributemodel';
 
 @Component({
     moduleId: module.id,
-    selector: 'attribute-details',
+    selector: '[attribute-row]',
     templateUrl: 'attribute.component.html'
 })
-export class AttributeComponent {
+export class AttributeComponent implements OnInit {
     @Input() form: FormGroup;
     @Input() attribute: AttributeModel;
 
     isValid(): boolean {
         let formControl = this.form.controls[this.attribute.name];
         return formControl.valid || formControl.pristine;
+    }
+
+    ngOnInit(): void {
+        
     }
 } 
